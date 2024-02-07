ee92c94dc1125301e8f9a71b8f08cbe3947f8ec9
Jack Hsu <jack.hsu@gmail.com>
2024-02-07 15:56:58 -0500

    update patch

--- scripts/patch.js
+++ scripts/patch.js
@@ -5,20 +5,13 @@ const content = fs.readFileSync(file).toString();
 if (!content.includes('PATCHED')) {
   const updated = content.replace(
     `const { resolveConfig } = await (0, _executorutils.loadViteDynamicImport)();`,
-    `console.log('>>> patched plugin 789');
-     const { resolveConfig } = await (0, _executorutils.loadViteDynamicImport)();`
+    `
+    // PATCHED
+    console.log('>>> patched plugin 10');
+    const { resolveConfig } = await (0, _executorutils.loadViteDynamicImport)();`
   );
   fs.writeFileSync(file, updated);
-console.log(fs.readFileSync(file).toString());
+  console.log(fs.readFileSync(file).toString());
 } else {
   console.log('already patched');
 }
-
-// const file2 = 'node_modules/@nx/vite/src/utils/executor-utils.js'
-// const content2 = fs.readFileSync(file2).toString();
-// const updated2 = content2.replace(
-//   `return import("vite")`,
-//   `// PATCHED
-//   return import("vite?t=" + Date.now())`
-// );
-// fs.writeFileSync(file2, updated2);
