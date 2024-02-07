5be8bba21c5285a8635efa242b042f8917c57244
Jack Hsu <jack.hsu@gmail.com>
2024-02-07 14:18:40 -0500

    update patch again

--- scripts/patch.js
+++ scripts/patch.js
@@ -6,14 +6,14 @@ const content = fs.readFileSync(file).toString();
 
 if (!content.includes('PATCHED')) {
   const updated = content.replace(
-    `const viteConfig = await resolveConfig({`,
+    `const { resolveConfig } = await (0, _executorutils.loadViteDynamicImport)();`,
     `// PATCHED - clear out CJS module cache
      console.log('PATCHED plugin');
      for (const key of Object.keys(require.cache)) {
        delete require.cache[key];
      }
      require('esbuild');
-     const viteConfig = await resolveConfig({`
+     const { resolveConfig } = await (0, _executorutils.loadViteDynamicImport)();`
   );
   fs.writeFileSync(file, updated);
 } else {
