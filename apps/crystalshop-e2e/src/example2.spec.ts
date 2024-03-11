fba7d22c9a0f5731a933f2e6c6fb3c2ff10f84b3
Juri <juri.strumpflohner@gmail.com>
2024-03-11 14:48:24 +0100

    flaky tests

--- apps/crystalshop-e2e/src/example2.spec.ts
+++ apps/crystalshop-e2e/src/example2.spec.ts
@@ -3,6 +3,12 @@ import { test, expect } from '@playwright/test';
 test('has title two', async ({ page }) => {
   await page.goto('/');
 
+  // make this test fail randomly to test flaky test re-running
+  // eslint-disable-next-line playwright/no-conditional-in-test
+  if (Math.random() > 0.5) {
+    throw new Error('Random failure');
+  }
+
   // Expect h1 to contain a substring.
   expect(await page.locator('h1').innerText()).toContain('Welcome');
 });
